5b37f40870bc1e3bc157e3e69c6e26676a2b2c2f
Elianti Piedad <jelianchaszer@gmail.com>
2022-02-25 01:58:32 +0800

    udpate our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-	<div className={`${size} menu-item`}>
+import {
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params }}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+const MenuItem = ({ title, imageUrl, size, linkUrl, router}) => (
+	<div className={`${size} menu-item`} onClick={() => router.navigate(linkUrl)}>
 		<div 
 			className='background-image'
 			style={{ 
@@ -16,4 +38,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 	</div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem)
